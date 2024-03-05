205ee944cf6869a7c06df0fd6f87ff0e478fac00
YonugJones <peterckerfoot@gmail.com>
2024-03-05 10:00:32 -0600

    completed tests and passed tests for capitalize, reverseString, calculator, caesarCipher, and analysisArray

--- src/functions.js
+++ src/functions.js
@@ -11,7 +11,6 @@ function reverseString(string) {
     const stringArray = string.split('');
     const reverseStringArray = [];
 
-    // eslint-disable-next-line no-plusplus
     for (let i = stringArray.length - 1; i >= 0; i--) {
         reverseStringArray.push(stringArray[i]);
     }
@@ -20,3 +19,74 @@ function reverseString(string) {
 }
 module.exports.reverseString = reverseString;
 
+class Calculator {
+    static add(a, b) {
+        return a + b;
+    }
+
+    static subtract(a, b) {
+        return a - b;
+    }
+
+    static multiply(a, b) {
+        return a * b;
+    }
+
+    static divide(a, b) {
+        if (b === 0) {
+            throw new Error('Division by zero');
+        }
+        return a / b;
+    }
+}
+module.exports.Calculator = Calculator;
+
+function caesarCipher(string, number) {
+    if (typeof string !== 'string' || typeof number !== 'number') return null;
+
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabetArray = alphabet.split('');
+
+    const cipherArray = [];
+
+    const lowerCaseString = string.toLowerCase();
+
+    for (let i = 0; i < lowerCaseString.length; i++) {
+        if (/[a-zA-Z]/.test(lowerCaseString[i])) {
+            const index = alphabetArray.indexOf(lowerCaseString[i]);
+            let newIndex = (index + number) % 26;
+            if (newIndex < 0) {
+                newIndex += 26;
+            }
+            const newChar = alphabetArray[newIndex].toUpperCase();
+            cipherArray.push(newChar);
+        } else {
+            cipherArray.push(lowerCaseString[i]);
+        }
+    }
+    return cipherArray.join('');
+}
+module.exports.caesarCipher = caesarCipher;
+
+function analyzeArray(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error('input must be array')
+    }
+    if (arr.length === 0) {
+        throw new Error('array must have length greater than 0')
+    }
+
+    const sum = arr.reduce((total, num) => total + num, 0);
+    const average = sum / arr.length;
+    const min = Math.min(...arr);
+    const max = Math.max(...arr);
+    const {length} = arr;
+
+    return {
+        average,
+        min,
+        max,
+        length
+    };
+}
+module.exports.analyzeArray = analyzeArray;
--- src/functions.test.js
+++ src/functions.test.js
@@ -1,20 +1,54 @@
-const { capitalize, reverseString } = require('./functions');
+const { capitalize, reverseString, Calculator, caesarCipher, analyzeArray } = require('./functions');
 
+// 1.
 test('return capitalized first letter string', () => {
     expect(capitalize('string')).toBe('String')
 });
 
+// 2.
 test('return reversed string', () => {
     expect(reverseString('string')).toBe('gnirts')
 });
 
+// 3.
 describe('Calculator', () => {
-    let calc;
 
-    beforeEach()
+    test('returns the sum of two numbers', () => {
+        expect(Calculator.add(5, 8)).toBe(13)
+    });
 
+    test('returns the difference of two numbers', () => {
+        expect(Calculator.subtract(10, 2)).toBe(8)
+    });
+
+    test('returns the product of two numbers', ()=> {
+        expect(Calculator.multiply(5, 5)).toBe(25)
+    })
+
+    test('returns the quotient of two numbers', () => {
+        expect(Calculator.divide(48, 12)).toBe(4)
+    })
 
 });
+
+// 4.
+test('return caesar ciphertext', () => {
+    expect(caesarCipher('Beware the Ides of March.', 7)).toBe('ILDHYL AOL PKLZ VM THYJO.')
+});
+
+// 5. 
+
+test('return option with analysis of array', () => {
+    const inputArray = [1,8,3,4,2,6];
+    const resultObject = analyzeArray(inputArray);
+    
+    expect(resultObject.average).toBe(4);
+    expect(resultObject.min).toBe(1);
+    expect(resultObject.max).toBe(8);
+    expect(resultObject.length).toBe(6);
+
+});
+
 
 
 
@@ -37,9 +71,9 @@ describe('Calculator', () => {
 
 // });
 
-// describe is a way to group your tests. It takes two arguments, a string (the name of the group) and a function (the actual test suite). 
-// It's not specifically for class names; you can use it to group any related tests together. In the provided example, 
-// we're using it to group tests related to the Calculator class.
+// describe is a way to group your tests. It takes two arguments, a string (the name of the group) 
+// and a function (the actual test suite). 
+
 
 
 // BEFOREEACH
@@ -51,7 +85,6 @@ describe('Calculator', () => {
 
 // beforeEach is a function provided by Jest (and other testing frameworks) 
 // that allows you to run some setup code before each test case in a describe block. 
-// In this case, we're using it to create a new instance of the Calculator class before each test case. 
 // This ensures that each test case operates on a clean instance of the Calculator class without interference from previous tests.
 
 
